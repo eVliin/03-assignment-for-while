72d969e3ce08f4a6357dcd43cd636c3171c676dc
Vinicius Lima Barreto <GON0141859@aluno.firjansenaisesi.com.br>
2022-06-15 10:18:04 -0300

    %

---
+++ script.js
@@ -0,0 +1,22 @@
+
+n = 0
+
+for (n; n < 11; n++) {
+    console.log(n)
+}
+
+while (n > 1) {
+    n--
+    console.log(n)
+}
+
+while (n < 100) {
+    console.log(n)
+    
+}
+
+n--
+
+for (n; n > 1; n = (n - 2)) {
+    console.log(n)
+}
